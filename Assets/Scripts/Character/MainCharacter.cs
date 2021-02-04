e3d060bcdcb77dc8c41bf11c8f2c56e6d3cebd2f
October_26 <ak2p12@gmail.com>
2021-02-04 22:21:52 +0900

    플레이어 점프 구현중

--- Assets/Scripts/Character/MainCharacter.cs
+++ Assets/Scripts/Character/MainCharacter.cs
@@ -9,11 +9,15 @@ public class MainCharacter : MonoBehaviour
     public KeyCode Key_Right;
     public KeyCode Key_Up;
     public KeyCode Key_Down;
+    public KeyCode Key_Jump;
 
     //========== 캐릭터 정보 ==========//
     public float MoveSpeed; //캐릭터 이동속도
-    float horizontal;
-    float vertical;
+    public float JumpPower; //점프 힘
+    float gravity; //현재 중력 
+    float horizontal;   //수평
+    float vertical; //수직
+    
 
     SpriteRenderer spriteRenderer;
     Animator animater;
@@ -31,18 +35,18 @@ public class MainCharacter : MonoBehaviour
         while (true)
         {
             horizontal = Input.GetAxis("Horizontal");
-            vertical = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
 
             transform.Translate(Vector3.right * (horizontal * MoveSpeed) * Time.deltaTime, Space.World);
 
-            //=====오른쪽 이동
+            //========== 오른쪽 이동
             if (horizontal > 0)
             {
                 animater.SetInteger("AnimState", 1);
                 spriteRenderer.flipX = false;
             }
 
-            //=====왼쪽 이동
+            //========== 왼쪽 이동
             else if (horizontal < 0)
             {
                 animater.SetInteger("AnimState", 1);
@@ -53,8 +57,10 @@ public class MainCharacter : MonoBehaviour
                 animater.SetInteger("AnimState", 0);
             }
 
-            if (Input.GetKey(Key_Up))
+            //========== 점프
+            if (Input.GetKey(Key_Up) || Input.GetKey(Key_Jump))
             {
+                animater.SetTrigger("Jump");
                 //transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime, Space.World);
             }
             if (Input.GetKey(Key_Down))

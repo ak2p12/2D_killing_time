18b0ce0238f603a04c6e852c289acf82c8e682d7
October_26 <ak2p12@gmail.com>
2021-02-07 22:07:44 +0900

    점프 구현중 점프시 충돌 구현중

--- Assets/Scripts/Character/MainCharacter.cs
+++ Assets/Scripts/Character/MainCharacter.cs
@@ -14,21 +14,63 @@ public class MainCharacter : MonoBehaviour
     //========== 캐릭터 정보 ==========//
     public float MoveSpeed; //캐릭터 이동속도
     public float JumpPower; //점프 힘
-    public float Gravity; //중력 
-    public float G_Acceleration; //중력 가속도
+    Vector3 a;
+    
+    float g_Acceleration; //중력 가속도
     float horizontal;   //수평
     float vertical; //수직
-    Vector3 playerLook; //캐릭터가 향하고 있는 방향
     bool isGround;    //캐릭터가 땅위에 서있는지 아닌지
+    bool isJump;    //캐릭터가 점프 했는지
 
     SpriteRenderer spriteRenderer;
     Animator animater;
+    Rigidbody2D rigid;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animater = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
         StartCoroutine(Update_Coroutine());
+    }
+
+    void Run()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        //========== 좌우 이동 계산
+        transform.position += new Vector3(horizontal, 0, 0) * (MoveSpeed * Time.deltaTime);
+
+        //========== 오른쪽 이동
+        if (horizontal > 0)
+        {
+            animater.SetInteger("AnimState", 1);
+            spriteRenderer.flipX = false;
+        }
+        //========== 왼쪽 이동
+        else if (horizontal < 0)
+        {
+            animater.SetInteger("AnimState", 1);
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animater.SetInteger("AnimState", 0);
+        }
+    }
+
+    void Jump()
+    {
+        //========== 점프
+        if ( (Input.GetKeyDown(Key_Up) || Input.GetKeyDown(Key_Jump)) && false == isJump)
+        {
+            animater.SetTrigger("Jump");
+            rigid.AddForce(new Vector2(0,JumpPower) , ForceMode2D.Impulse);
+            isGround = false;
+            isJump = true;
+        }
+
+        animater.SetFloat("G_Acceleration", rigid.velocity.y);
     }
 
     IEnumerator Update_Coroutine()
@@ -36,55 +78,35 @@ public class MainCharacter : MonoBehaviour
         
         while (true)
         {
-            horizontal = Input.GetAxis("Horizontal");
-            //vertical = Input.GetAxis("Vertical");
+            Run();
+            Jump();
 
-            //========== 좌우 이동 계산
-            transform.Translate( Vector3.right * (horizontal * MoveSpeed) * Time.deltaTime , Space.World);
-
-            //========== 점프 및 중력 계산
-            //transform.Translate(Vector3.right * (horizontal * MoveSpeed) * Time.deltaTime, Space.World);
-
-            //========== 오른쪽 이동
-            if (horizontal > 0)
-            {
-                animater.SetInteger("AnimState", 1);
-                spriteRenderer.flipX = false;
-            }
-
-            //========== 왼쪽 이동
-            else if (horizontal < 0)
-            {
-                animater.SetInteger("AnimState", 1);
-                spriteRenderer.flipX = true;
-            }
-            else
-            {
-                animater.SetInteger("AnimState", 0);
-            }
-
-            //========== 점프
-            if (Input.GetKey(Key_Up) || Input.GetKey(Key_Jump))
-            {
-                animater.SetTrigger("Jump");
-                //transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime, Space.World);
-            }
-            //if (Input.GetKey(Key_Down))
-            //{
-            //    //transform.Translate(Vector3.left * MoveSpeed * Time.deltaTime, Space.World);
-            //}
-
-            animater.SetBool("Ground", isGround);
             yield return null;
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 9)
         {
-            int i = 0;
+            isGround = true;
+            isJump = false;
+            animater.SetBool("Ground", isGround);
         }
-        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            if (!isJump)
+            {
+                isGround = false;
+                animater.SetBool("Ground", isGround);
+            }
+        }
     }
 }

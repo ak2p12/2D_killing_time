a55ea0aa16da09ea16b005dae722ae77a81a2998
October_26 <ak2p12@gmail.com>
2021-03-23 00:36:16 +0900

    게이지바 작업중

---
+++ Assets/Scripts/GaugeBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaugeBar : MonoBehaviour
+{
+    public GameObject TargetObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (TargetObject.tag == "Player")
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
